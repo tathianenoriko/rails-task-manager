c04197715fdc907973b1aef4943589d7a9249fcd
Tathiane Noriko <tathianenoriko@gmail.com>
2025-02-25 17:19:18 -0300

    Rails-Task_Manager

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,27 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  root to: 'tasks#index'
+
+  # rota para listar todas as tarefas
+  get 'tasks', to: 'tasks#index'
+
+  # rota para exibir o form
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+
+  # rota para exibir os detalher de uma tarefa específica
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # rota para criar a nova tarefa
+  post 'tasks', to: 'tasks#create'
+
+  # rota para exibir o form para edição
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  # rota para atualizar uma tarefa específica
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # rota para deletar a tarefa
+  delete 'tasks/:id', to: 'tasks#destroy'
   # root "posts#index"
 end
